07471c3f8d0dbbaf95a8da3a7b8a8bd6bbaacbbe
Maria <mliberov@gmail.com>
2015-02-23 22:36:44 -0500

    Add hide last row and show last row functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,3 +20,28 @@ function disableButton() {
   var button = document.getElementById("update_button");
   setTimeout(function() {button.disabled = true;}, 1);
 }
+
+
+function hideRow() {
+  if (document.getElementById("show_hide_row") != "null") {
+    var row = document.getElementById("show_hide_row");
+    var last = row.lastElementChild;
+    last.style.display = 'none';
+  }
+}
+
+function addRow() {
+  if (document.getElementById("show_hide_row") != "null") {
+    var row = document.getElementById("show_hide_row");
+    var last = row.lastElementChild;
+    last.style.display = 'block';
+  }
+}
+
+
+
+//= When a Delete button is clicked, hide the row to which it belongs, but mark the hidden _destroy field in that row as truthy.
+
+
+
+//= Back on the course detail page, you'll notice that clicking on the various nav buttons in the blue box will cause the page to snap to that section. Implement smooth scrolling instead.
